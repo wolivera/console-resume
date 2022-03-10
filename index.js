0f3dc8f689bd02fad2fa778f41b1ca4b194d8dd8
Williams Olivera <wolivera@zircon.tech>
2022-03-10 09:32:27 -0300

    enhance: updated resume data

--- index.js
+++ index.js
@@ -2,17 +2,17 @@
   const myself = {
     getAbout: '$ cd Documents/Myself^1000\n$ cat about.txt\n',
     description:
-      'Hi, I’m Williams.`\n`  I’m a Software Engineer from Uruguay an small country near latitude 33.`\n`  I’ve been working the past 6 years in Software Development, being part of multiple`\n`  projects and building a great variety of programs using different techonologies.`\n`  I’m passionate about Piknk Floyd, traveling and Asimov books.',
+      'Hi, I’m Williams.`\n`  I’m a Software Engineer from Uruguay, an small country near latitude 33.`\n`  I’ve been working the past 12 years in Software Development, being part of multiple`\n`  projects and building a great variety of programs using different techonologies, nowadays working as the CTO of ZirconTech.`\n`  I’m a passionate music listener, loving to travel and having long lectures of Asimov books.',
     getCompetencies:
       '\n\n$ sudo apt-get competencies\n ` Fetching from source...`\n ` Installing libraries...`\n\n`  * Loading core competencies ...`\n',
     competencies:
       '*** Backend API architecture`\n`  *** AWS DevOps engineering`\n`  *** Node JS for performant apps`\n`  *** Blockchain development`\n`  *** Relational and NoSQL database at scale`\n`  *** Unit/End to End testing`\n`  *** Agile methodologies`\n`  *** Troubleshooting`\n`  *** Communication / Collaboration `\n\n',
     getLanguages: '* Loading languages, frameworks and tools ... `\n',
     languages:
-      '*** Javascript, SQL, Solidity, Ruby, HTML, CSS, Python, Kotlin, Java`\n`  *** NodeJS, ReactJS, Express, Serverless, Redux, React Native, Sequelize, Ethereum Network, Corda, Rails, AngularJS, Flask`\n`  *** AWS, Heroku, Firebase, Digital Ocean, GCE, Postgres, MySQL, MongoDB, Redis, Git, Mocha, Jasmine, Nginx, Webpack`\n`  *** Auth0, TravisCI, Jira, Docker, Zoho, Stripe, BrainTree `\n\n',
+      '*** Javascript, SQL, Solidity, Ruby, HTML, CSS, Python, Dart, Go`\n`  *** NodeJS, ReactJS, NestJS, Serverless, Redux, React Native, Sequelize, Ethereum Network, Corda, Rails, AngularJS, Flask`\n`  *** AWS, Heroku, Firebase, Digital Ocean, GCE, Postgres, MySQL, MongoDB, Redis, Git, Mocha, Jasmine, Nginx, Webpack`\n`  *** Auth0, TravisCI, Jira, Docker, Zoho, Stripe, BrainTree `\n\n',
     getSources: '$ mount /dev/hda2 /external/sources\n\n',
     sources:
-      '1) Linkedin: https://www.linkedin.com/in/williams-olivera `\n` 2) Github: https://github.com/wolivera`\n` 3) Medium: https://medium.com/@liamsolivera \n\n\n\n\n',
+      '1) Linkedin: https://www.linkedin.com/in/williams-olivera `\n` 2) Github: https://github.com/wolivera `\n` 3) Twitter: https://twitter.com/liams_o `\n` 4) Medium: https://medium.com/@liamsolivera \n\n\n\n\n',
     getContactMe: "$ grep 'contact' /usr\n\n",
     contactMe: 'Want to chat? Drop me a line at liamsolivera@gmail.com',
   };
